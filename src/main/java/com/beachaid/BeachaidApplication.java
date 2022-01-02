0c1ead59bfa53cce8b4e904749a8a2464a0f2651
wrang308 <markus.wrang_94@hotmail.com>
2022-01-02 02:15:51 +0100

    first commit

---
+++ src/main/java/com/beachaid/BeachaidApplication.java
@@ -0,0 +1,13 @@
+package com.beachaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeachaidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeachaidApplication.class, args);
+	}
+
+}
